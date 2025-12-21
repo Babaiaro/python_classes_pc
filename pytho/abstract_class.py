5bcd038e47517e877dd2dc6a6ccffeff0b398d3b
Babaiar <119310971+Babaiaro@users.noreply.github.com>
2025-12-20 22:23:49 -0600

    Add files via upload

---
+++ pytho/abstract_class.py
@@ -0,0 +1,28 @@
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC): 
+    
+
+    @abstractmethod
+    def go(self):
+        pass
+
+
+    @abstractmethod
+    def stop(self):
+        pass
+
+
+class Car(Vehicle):
+
+    def go(self):
+        print("You drive the car")
+
+    def stop(self):
+        print("You stop the car")
+
+
+car = Car()
+
+car.go()
+car.stop()
